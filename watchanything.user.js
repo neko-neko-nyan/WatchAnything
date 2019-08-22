6e6be11d67d464b1cfe18463def3a2d6da888e19
Pasha13666 <Pasha13666@users.noreply.github.com>
2019-08-22 19:07:41 +0300

    Make 1.1.2 stable

--- watchanything.user.js
+++ watchanything.user.js
@@ -1,184 +1,130 @@
 // ==UserScript==
 // @name         WatchAnything
-// @namespace    https://github.com/Pasha13666
-// @version      1.0.2
-// @description  [shikimori.org] Кнопка открытия случайного аниме из списка
+// @namespace    https://openuserjs.org/users/Pasha13666
+// @version      1.1.2
+// @description  [shikimori.one] Кнопка открытия случайного аниме из списка
 // @author       Pasha13666
-// @homepageURL  https://github.com/Pasha13666/WatchAnything
-// @updateURL    https://openuserjs.org/meta/Pasha13666/WatchAnything.meta.js
-// @match        https://shikimori.org/*
+// @match        http://shikimori.one/*
+// @match        https://shikimori.one/*
 // @match        http://shikimori.org/*
-// @require      https://code.jquery.com/jquery-3.3.1.min.js
-// @require      https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js
-// @grant        unsafeWindow
+// @match        https://shikimori.org/*
+// @updateURL    https://openuserjs.org/meta/Pasha13666/WatchAnything.meta.js
+// @homepageURL  https://github.com/Pasha13666/WatchAnything
+// @run-at       document-body
 // @license      MIT
-// @copyright    2018, Pasha__kun (https://github.com/Pasha13666)
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_log
+// @copyright    2018-2019, Pasha13666 (https://github.com/Pasha13666)
 // ==/UserScript==
 
-var $ = jQuery.noConflict(true),
-    lUrl, lData, lKind,
-    $doc = $(document);
+const LIST_NAMES = ['planned', 'watching', 'completed', 'on_hold', 'dropped'];
 
-if (window.jQuery === undefined){
-    window.jQuery = unsafeWindow.jQuery;
+function WatchAnything(){
+    this.onRandomClicked = this.onRandomClicked.bind(this);
+    this.triggerChange = this.triggerChange.bind(this);
 }
 
-function parseUrl(){
-    if (lUrl === window.location.pathname)
-        return lData;
-
-    var url = window.location.pathname.split('/');
-    if (url.length < 4 || url[2] != 'list' || (url[3] != 'anime' && url[3] != 'manga' && url[3] != 'ranobe'))
-        return undefined;
-
-    lUrl = window.location.pathname;
-    lData = {};
-    lKind = url[3];
-    for (var i = 4; i < url.length; i+=2) {
-        var k = url[i], v = url[i + 1];
-        if (k === 'order-by')
-            continue;
-
-        lData[k] = v;
-    }
-
-    lData.censored = false;
-    lData.limit = 50;
-    return lData;
-}
-
-function getAll(kind, data, page, csrf, mylist, fn){
-    var data2 = new Object(data);
-    data2.page = page;
-    data2.mylist = mylist;
-
-    $.ajax('https://shikimori.org/api/' + kind + 's', {
-        dataType: 'json',
-        data: data2,
-        headers: {
-            'X-CSRF-Token': csrf,
-            'X-Requested-With': 'XMLHttpRequest',
-        },
-        success: function(cnt){
-            if (cnt.length === 50)
-                getAll(kind, data, page + 1, csrf, mylist, function(c){
-                    fn(cnt.concat(c));
-                });
-            else fn(cnt);
-        }
-    });
-}
-
-function nameOf(i){
-    switch(i){
-        case 0: return 'planned';
-        case 1: return 'watching';
-        case 2: return 'completed';
-        case 3: return 'on_hold';
-        case 4: return 'dropped';
-    }
-}
-
-$.expr[':']['has-data'] = function(el, i, match) {
-        return $(el).data(match[3]) !== undefined;
-};
-
-$doc.on('click', '.collapse', function(){
-    var $this = $(this),
-        $parent = $this.parent(),
-        placeholder = $parent.next().next(),
-        element = $parent.parent().next(),
-        shown = !!$this.html().match(/\u0441\u0432\u0435\u0440\u043d\u0443\u0442\u044c|\u0441\u043f\u0440\u044f\u0442\u0430\u0442\u044c|collapse|hide/);
-
-    $this.toggleClass("triggered", shown);
-
-    while (element.last().next().hasClass("collapse-merged"))
-        element = element.add(n.last().next());
-
-    if (element.length > 1)
-        element = element.filter(":not(.collapse-ignored)");
-
-    if (shown){
-        placeholder.show();
-        element.hide();
-    } else {
-        element.show();
-        placeholder.hide();
-    }
-
-    $this.html(shown ? $this.html().replace("\u0441\u0432\u0435\u0440\u043d\u0443\u0442\u044c", "\u0440\u0430\u0437\u0432\u0435\u0440\u043d\u0443\u0442\u044c")
-            .replace("\u0441\u043f\u0440\u044f\u0442\u0430\u0442\u044c", "\u043f\u043e\u043a\u0430\u0437\u0430\u0442\u044c")
-            .replace("hide", "show")
-            .replace("collapse", "expand")
-        : $this.html()
-            .replace("\u0440\u0430\u0437\u0432\u0435\u0440\u043d\u0443\u0442\u044c", "\u0441\u0432\u0435\u0440\u043d\u0443\u0442\u044c")
-            .replace("\u043f\u043e\u043a\u0430\u0437\u0430\u0442\u044c", "\u0441\u043f\u0440\u044f\u0442\u0430\u0442\u044c")
-            .replace("show", "hide")
-            .replace("expand", "collapse"));
-
-    var s = $this.data("collapse-name") + ";",
-        i = Cookies.get("collapses") || "";
-    if ((i.indexOf(s) === -1) === shown)
-        Cookies.set("collapses", shown? i + s: i.replace(s, ""), {
-            expires: 730,
-            path: "/"
-        });
-
-    return placeholder.next().trigger("show");
-});
-
-$doc.on('click auxclick', '.open-random', function(e){
-    var newTab = e.which !== 1;
-
-    parseUrl();
-    getAll(lKind, lData, 1, $('[name=csrf-token]').attr('content'), $(this).data('list-name'), function(c){
-        c = c[Math.floor(Math.random() * c.length)];
-        console.log(c);
-
-        if (newTab) window.open('//shikimori.org' + c.url, '_blank', '');
-        else window.location.pathname = c.url;
-    });
-
-    if (newTab) return false;
-});
-
-function start(){
-    if (parseUrl() === undefined)
+WatchAnything.prototype.triggerChange = function(){
+    if (!document.body || !document.body.classList.contains("p-user_rates"))
         return;
 
-    for (var i = 0; i < 5; i++){
-        var it = $('.status-' + i + ' .b-options-floated');
-        if (it.hasClass('with-random'))
-            continue;
+    GM_log("[WatchAnything] Starting / loading...");
+    if (location.href !== this.prevUrl) {
+        this.prevUrl = location.href;
+        this.onUrlChanged();
+    }
+    this.bindToLists();
+}
 
-        it.parent().children(":last").click(function(){
-            $(this).prev().prev().children(":last").click();
-        });
-
-        $('<div/>').addClass('b-options-floated with-random').append(
-            $('<a/>').text('Рандомное аниме').data('list-name', nameOf(i)).addClass('open-random')
-        ).append(
-            $('<a/>').text(it.children(":first").text()).data("collapse-name", i).addClass('collapse')
-        ).replaceAll(it);
+WatchAnything.prototype.onUrlChanged = function (){
+    const url = window.location.pathname.split('/');
+    this.data = {};
+    this.kind = url[3];
+    for (let i = 4; i < url.length; i+=2) {
+        const k = url[i], v = url[i + 1];
+        if (k !== 'order-by')
+            this.data[k] = v;
     }
 
-    $('.collapse:has-data(collapse-name)').each(function(){
-        var $this = $(this),
-            shown = $this.parent().parent().next().css('display') !== 'none',
-            s = $this.data("collapse-name") + ";",
-            i = Cookies.get("collapses") || "";
+    this.data.censored = false;
+    this.data.limit = 50;
 
-        if ((i.indexOf(s) === -1) !== shown)
-            $this.click();
-    });
+    this.csrf = document.querySelector('meta[name=csrf-token]').content;
+
+    document.removeEventListener('click', this.onRandomClicked);
+    document.addEventListener('click', this.onRandomClicked);
+    document.removeEventListener('auxclick', this.onRandomClicked);
+    document.addEventListener('auxclick', this.onRandomClicked);
 }
 
-$doc.ready(start);
+WatchAnything.prototype.bindToLists = function (){
+    for (let i = 0; i < 5; i++){
+        const $header = document.querySelector('.status-' + i);
+        if (!$header || $header.children[1].classList.contains('wa-link'))
+            continue;
 
-try {
-    jQuery(document).on('page:load turbolinks:load postloader:success', start);
-} catch(e){
-    alert('Невозможно запустить WatchAnything в Firefox. Удалите скрипт в панели GreaseMonkey.\nCant run WatchAnything in Firefox. Please, remove script from GreaseMonkey panel.');
-    console.error('Startup failed!', e);
+        const $link = document.createElement('div');
+
+        $link.classList = "b-options-floated wa-link";
+        $link.innerHTML = '<span class="action">Рандом</span>';
+        $link.dataset.listName = LIST_NAMES[i];
+
+        $header.children[1].before($link);
+    }
 }
 
+WatchAnything.prototype.onRandomClicked = function(e) {
+    if(e.path.length >= 2 && e.path[1].classList.contains('wa-link')) {
+        var newTab = e.which !== 1;
+        const $link = e.path[1];
+
+        this.loadList(1, $link.dataset.listName, function(c){
+            c = c[Math.floor(Math.random() * c.length)];
+            GM_log("[WatchAnything] Opening random selected:", c.russian || c.name);
+
+            if (newTab) window.open(location.origin + c.url, '_blank', '');
+            else window.location.pathname = c.url;
+        });
+
+        if (newTab) e.stopImmediatePropagation();
+    }
+}
+
+WatchAnything.prototype.loadList = function(page, mylist, fn){
+    var ajax = new XMLHttpRequest();
+    var data = new Object(this.data);
+    data.page = page;
+    data.mylist = mylist;
+
+    ajax.open("GET", location.origin + '/api/' + this.kind + 's?' + Object.keys(data).map(key => key + '=' + data[key]).join('&'), true);
+    ajax.setRequestHeader('X-CSRF-Token', this.csrf);
+    ajax.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    ajax.setRequestHeader('X-Userscript', 'WatchAnything');
+
+    ajax.onreadystatechange = () => {
+        if (ajax.readyState != 4 || ajax.status != 200)
+            return;
+
+        const cnt = JSON.parse(ajax.responseText);
+        if (cnt.length === 50) {
+            if (page % 5 === 0) setTimeout(() => this.loadList(page + 1, mylist, function(c){
+                fn(cnt.concat(c));
+            }), 1000);
+            else this.loadList(page + 1, mylist, function(c){
+                fn(cnt.concat(c));
+            });
+        } else fn(cnt);
+    }
+
+    ajax.send();
+}
+
+document.head.append(GM_addStyle("header>.b-options-floated { position: inherit; float: right; margin: auto 10px; }"));
+const wa = new WatchAnything;
+
+document.addEventListener('ready', wa.triggerChange);
+document.addEventListener('page:load', wa.triggerChange);
+document.addEventListener('turbolinks:load', wa.triggerChange);
+wa.triggerChange();
